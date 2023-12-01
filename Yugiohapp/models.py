589091a163387fdfe1e1c29466f05c07c96494bd
ApenasUDev <francimaralexandredt@gmail.com>
2023-12-01 18:53:05 -0300

    corrigindo erros no models

--- Yugiohapp/models.py
+++ Yugiohapp/models.py
@@ -5,7 +5,7 @@ class Usuarios(models.Model):
     nome_usuario = models.CharField(max_length=255,unique=True)
     email = models.CharField(max_length=255,unique=True)
     senha = models.CharField(max_length=25)
-    data_cadastro = models.DateField(auto_created=True)
+    data_cadastro = models.DateField()
     img_perfil = models.ImageField()
     def __str__(self) -> str:
         return self.nome_usuario
@@ -16,10 +16,10 @@ class Favoritos(models.Model):
     def __str__(self) -> str:
         return self.nome_card
 class Avaliacao(models.Model):
-    usuario = models.OneToOneField(Usuarios,on_delete=True)
+    usuario = models.OneToOneField(Usuarios,on_delete=models.CASCADE)
     id_card = models.IntegerField()
     nome_card = models.CharField(max_length=125)
-    estrelas = models.IntegerField(min=1,max=5)
+    estrelas = models.IntegerField()
     
     def __str__(self) -> str:
         return self.nome_card
