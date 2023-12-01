bad855b49ed00dc0a3c5dcfa96638dd337b05943
ApenasUDev <francimaralexandredt@gmail.com>
2023-12-01 18:44:49 -0300

    criando models

--- Yugiohapp/models.py
+++ Yugiohapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nome_usuario = models.CharField(max_length=255,unique=True)
+    email = models.CharField(max_length=255,unique=True)
+    senha = models.CharField(max_length=25)
+    data_cadastro = models.DateField(auto_created=True)
+    img_perfil = models.ImageField()
+    def __str__(self) -> str:
+        return self.nome_usuario
+class Favoritos(models.Model):
+    id_card = models.IntegerField()
+    nome_card = models.CharField(max_length=125)
+    
+    def __str__(self) -> str:
+        return self.nome_card
+class Avaliacao(models.Model):
+    usuario = models.OneToOneField(Usuarios,on_delete=True)
+    id_card = models.IntegerField()
+    nome_card = models.CharField(max_length=125)
+    estrelas = models.IntegerField(min=1,max=5)
+    
+    def __str__(self) -> str:
+        return self.nome_card
+    
